a02ba2e4fb25162065720a975d05613d427a7b6a
gethsun1 <gethsun09@gmail.com>
2020-06-07 01:07:11 +0300

    Initial Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/africansun/Documents/GKM/Projects/Gethsun-P/dj_gethsun/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
